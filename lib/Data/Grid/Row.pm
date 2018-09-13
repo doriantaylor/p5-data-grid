994af09b6a47e52d8cff5c800ca558737003c616
Dorian Taylor <dorian.taylor.lists@gmail.com>
2018-09-13 14:43:07 -0700

    version fix

--- lib/Data/Grid/Row.pm
+++ lib/Data/Grid/Row.pm
@@ -1,6 +1,6 @@
 package Data::Grid::Row;
 
-use 5.012;
+use 5.014;
 use strict;
 use warnings FATAL => 'all';
 
@@ -19,11 +19,11 @@ Data::Grid::Row - Row implementation for Data::Grid::Table
 
 =head1 VERSION
 
-Version 0.02_01
+Version 0.06
 
 =cut
 
-our $VERSION = '0.02_01';
+our $VERSION = '0.06';
 
 =head1 SYNOPSIS
 
